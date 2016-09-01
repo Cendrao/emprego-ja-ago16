a56bf5b57fce05f2ccb791f660762b3668cc1bde
Matheus e Thiago <matheuscendrao@gmail.com>
2016-08-31 21:33:12 -0300

    User must create valid jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,12 +5,18 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
-    @companies = Company.all
+    set_companies
   end
 
   def create
-    job = Job.create job_params
-    redirect_to job
+    @job = Job.new job_params
+    if @job.save
+      redirect_to @job
+    else
+      set_companies
+      flash[:errors] = 'Não foi possível criar a vaga'
+      render :new
+    end
   end
 
   private
@@ -20,4 +26,10 @@ class JobsController < ApplicationController
       [:title, :location, :category, :company_id, :description, :featured]
     )
   end
+
+  def set_companies
+    @companies = Company.all
+  end
+
+
 end
